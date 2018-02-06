ad234b37bcb05ab87cfb9ceebbc7ee8adf4d3aef
0l3shka <q0l3shka@gmail.com>
2018-02-06 14:52:34 +0700

    Create new project, import class

---
+++ ProcessMultipleExpceptions/Car.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Car
+    {
+        public const int MaxSpeed = 100;
+
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        private Radio theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(string name,int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+                    CarIsDeadException ex=
+                        new CarIsDeadException(string.Format("{0} has overheated!",PetName),
+                        "You have a lead foot", DateTime.Now);
+                    ex.HelpLink = "http://www.CarsRU.com";
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+
+    }
+
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            if (on)
+                Console.WriteLine("Jamming");
+            else
+                Console.WriteLine("Quiet time...");
+        }
+    }
+
+
+    public class CarIsDeadException : ApplicationException
+    {
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException() { }
+        public CarIsDeadException(string message,string cause,DateTime time)
+            :base (message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+
+        }
+
+        
+    }
+}
