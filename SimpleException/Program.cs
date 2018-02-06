17771a6e27a7c41846015b2632fd87c99a1eaf8a
0l3shka <q0l3shka@gmail.com>
2018-02-06 14:01:14 +0700

    +main

--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -10,6 +10,13 @@ namespace SimpleException
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("****** simple Exception Example ******");
+            Console.WriteLine("=> Creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+            for (int i = 0; i < 10; i++)
+                myCar.Accelerate(10);
+            Console.ReadLine();
         }
     }
 }
